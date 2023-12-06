624e4587a7cc6246d67c0ce2e7a94e48205182bf
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2023-12-06 09:00:16 +0200

    Style: Rubocop autocorrect Style/FileWrite

--- lib/aoc_rb/cli.rb
+++ lib/aoc_rb/cli.rb
@@ -25,27 +25,27 @@ module AocRb
       bin_path = File.join(bin_dir, "aoc")
       bin_template = File.join(File.dirname(__FILE__), "../../templates/bin/aoc")
       FileUtils.mkdir_p bin_dir
-      File.open(bin_path, "w") { |f| f.write(File.read(bin_template)) }
+      File.write(bin_path, File.read(bin_template))
 
       shared_dir = File.join(project_dir, "challenges", "shared")
       solution_path = File.join(shared_dir, "solution.rb")
       solution_template = File.join(File.dirname(__FILE__), "../../templates/solution_base.rb")
       FileUtils.mkdir_p shared_dir
-      File.open(solution_path, "w") { |f| f.write(File.read(solution_template)) }
+      File.write(solution_path, File.read(solution_template))
 
       spec_dir = File.join(project_dir, "spec")
       spec_helper_path = File.join(spec_dir, "spec_helper.rb")
       spec_helper_template = File.join(File.dirname(__FILE__), "../../templates/spec/spec_helper.rb")
       FileUtils.mkdir_p spec_dir
-      File.open(spec_helper_path, "w") { |f| f.write(File.read(spec_helper_template)) }
+      File.write(spec_helper_path, File.read(spec_helper_template))
 
       env_template_path = File.join(project_dir, ".env-template")
       env_template = File.join(File.dirname(__FILE__), "../../templates/.env-template")
-      File.open(env_template_path, "w") { |f| f.write(File.read(env_template)) }
+      File.write(env_template_path, File.read(env_template))
 
       gemfile_dst = File.join(project_dir, "Gemfile")
       gemfile_src = File.join(File.dirname(__FILE__), "../../templates/Gemfile")
-      File.open(gemfile_dst, "w") { |f| f.write(File.read(gemfile_src)) }
+      File.write(gemfile_dst, File.read(gemfile_src))
     end
 
     desc "version", "prints the current installed version of AocRb"
