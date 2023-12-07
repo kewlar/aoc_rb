83019b734c74dc71f74aae8d132e1b5fb4dd8dca
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2023-12-07 22:04:59 +0200

    Change: configure RSpec formatter to 'doc' and add some more tweaks.

--- templates/spec/spec_helper.rb
+++ templates/spec/spec_helper.rb
@@ -9,4 +9,7 @@ Dir[File.join(__dir__, "..", "challenges", "20*", "**", "*.rb")].each do |file|
 end
 
 RSpec.configure do |config|
+  config.default_formatter = "doc"
+  config.example_status_persistence_file_path = ".rspec_status"
+  config.filter_run_when_matching :focus
 end
