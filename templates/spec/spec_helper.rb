0a42d32f42f1a846a7f3ec23344aa4fc66d7198f
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2023-12-15 21:03:17 +0200

    Code style: replaced File.dirname(__FILE__) to __dir__.

--- templates/spec/spec_helper.rb
+++ templates/spec/spec_helper.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-Dir[File.join(File.dirname(__FILE__), '..', 'challenges', 'shared', '**', '*.rb')].each do |file|
+Dir[File.join(__dir__, '..', 'challenges', 'shared', '**', '*.rb')].each do |file|
   require file
 end
 
-Dir[File.join(File.dirname(__FILE__), '..', 'challenges', '20*', '**', '*.rb')].each do |file|
+Dir[File.join(__dir__, '..', 'challenges', '20*', '**', '*.rb')].each do |file|
   require file
 end
 
 RSpec.configure do |config|
-  config.default_formatter = "doc"
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.default_formatter = 'doc'
+  config.example_status_persistence_file_path = '.rspec_status'
   config.filter_run_when_matching :focus
 end
